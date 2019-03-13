87b52287957d85e3dab8d02443d35115cf3b869f
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-13 17:22:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,5 @@
 ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-def alphabetize(arr)
-  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+def alpheabetize(arr)
+
 end
